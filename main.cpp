c9e5e5a5618ac11dcf0203503af9f44e1b41c0d9
Raynell2005 <littlefieldraynell@gmail.com>
2024-10-16 12:20:07 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\n Welcome to my Zoo Parsing program!" << std::endl;
+    string sampleInput = "4 year old female hyena, born in spring, tan color, 70 pounds,from Friguia Park, Tunisia"
+String mySpecies = String();
+    string arrayOfWords {5};
+cout << sampleInput << endl;
+
+    return 0;
+
+    cout << "\n in the first group and word "<< j << " is:" << word;
+    j++;
+}
